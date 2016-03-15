dcea28a2379dfe8d90a2fc551be51a77c03770a6
jref <aleksandrnvk@ukr.net>
2016-03-15 17:51:16 +0200

    Update Shape.java

--- src/com/brainacad/oop/testshapes/Shape.java
+++ src/com/brainacad/oop/testshapes/Shape.java
@@ -1,11 +1,11 @@
 package com.brainacad.oop.testshapes;
 
-public class Shape {
+public class Shape { //should be abstract
     private String shapeColor;
     public String getShapeColor() {   return shapeColor; }
     public void setShapeColor(String shapeColor) {  this.shapeColor = shapeColor; }
 
-    public double calcArea(){double area=0.0 ; System.out.println("Shape area is: "+area); return area;}
+    public double calcArea(){double area=0.0 ; System.out.println("Shape area is: "+area); return area;} //method should be abstract
     public void Shape(String shapeColor){
         setShapeColor(shapeColor);}
 
